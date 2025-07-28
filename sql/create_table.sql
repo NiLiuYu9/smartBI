5c1b58f432f2eacf67ee01a42e382c1ce95c0fac
advent.ni <nly2455158786@163.com>
2025-07-28 15:37:47 +0800

    更新表创建文件

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,39 +1,50 @@
-# 建表脚本
-# @author <a href="https://github.com/liyupi">程序员鱼皮</a>
-# @from <a href="https://yupi.icu">编程导航知识星球</a>
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- 创建库
-create database if not exists yubi;
+-- ----------------------------
+-- Table structure for chart
+-- ----------------------------
+DROP TABLE IF EXISTS `chart`;
+CREATE TABLE `chart`  (
+                          `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+                          `goal` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '分析目标',
+                          `chartType` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图表类型',
+                          `genChart` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '生成的图表数据',
+                          `genResult` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '生成的分析结论',
+                          `userId` bigint NULL DEFAULT NULL COMMENT '创建用户 id',
+                          `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                          `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                          `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+                          `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '图表名称',
+                          `status` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'wait' COMMENT 'wait,running,succeed,failed',
+                          `execMessage` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '执行信息',
+                          PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1949725546535456771 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '图表信息表' ROW_FORMAT = Dynamic;
 
--- 切换库
-use yubi;
+SET FOREIGN_KEY_CHECKS = 1;
 
--- 用户表
-create table if not exists user
-(
-    id           bigint auto_increment                  comment 'id' primary key,
-    userAccount  varchar(256)                           not null comment '账号',
-    userPassword varchar(512)                           not null comment '密码',
-    userName     varchar(256)                           null comment '用户昵称',
-    userAvatar   varchar(1024)                          null comment '用户头像',
-    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
-    index idx_userAccount (userAccount)
-    ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 图表表
-create table if not exists chart
-(
-    id           bigint auto_increment comment 'id' primary key,
-    goal				 text  null comment '分析目标',
-    chartData    text  null comment '图表数据',
-    chartType	   varchar(128) null comment '图表类型',
-    genChart		 text	 null comment '生成的图表数据',
-    genResult		 text	 null comment '生成的分析结论',
-    userId       bigint null comment '创建用户 id',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除'
-) comment '图表信息表' collate = utf8mb4_unicode_ci;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+                         `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+                         `userAccount` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '账号',
+                         `userPassword` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+                         `userName` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户昵称',
+                         `userAvatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户头像',
+                         `userRole` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'user' COMMENT '用户角色：user/admin',
+                         `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                         `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                         `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '是否删除',
+                         PRIMARY KEY (`id`) USING BTREE,
+                         INDEX `idx_userAccount`(`userAccount` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1945668955297398786 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
